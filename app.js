f6f89339016bccf8475d2a1193c8e07ea8139523
riiamri <syaeful.amri@dataon.com>
2022-08-21 21:36:44 +0700

    add new manga endpoint

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const PORT = process.env.PORT || 3000
 const ScrapeController = require('./controllers/scrape.controller')
 const ScrapeNewsController = require('./controllers/scrapenews.controller')
+const ScrapeMangaController = require('./controllers/scrapemanga.controller')
 
 app.use(cors())
 app.use(helmet())
@@ -19,6 +20,11 @@ app.get('/search',ScrapeController.searchAnime)
 //anime news jurnalotaku
 app.get('/animenews', ScrapeNewsController.fetchNews)
 app.get('/animenews/detail', ScrapeNewsController.fetchDetailNews)
+
+// manga
+app.get('/mangalist', ScrapeMangaController.fetchManga)
+app.get('/manga/detail', ScrapeMangaController.fetchMangaDetail)
+app.get('/manga/watcher', ScrapeMangaController.fetchMangaWatcher)
 
 app.listen(PORT, ()=>{
     console.log('Server connected on port'+PORT)
